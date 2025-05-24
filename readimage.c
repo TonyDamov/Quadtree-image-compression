6af2f2926b6eb4d65431a23b3715b1c7818dd040
TonyDamov <antoan.damov@gmail.com>
2025-05-24 19:30:49 +0300

    Create readimage.c
    Created readimage.c. It opens the image, reads its contents and parses it into a 2d array of pixels.

---
+++ readimage.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "quadtree.h"
+// Working with .ppm files since its contents as easiest to read
+//.ppm saves the width and height of the image as a string which represents a number 
+unsigned long read_num(FILE * image) {
+    char buff[8];
+    int i = 0;
+    do {
+        buff[i] = fgetc(image);
+        i++;
+    } while(buff[i-1] != 10 && buff[i-1] != 32 && i < sizeof(buff));
+    buff[i-1] = '\0';
+    return strtoul(buff, NULL, 10);
+}
+
+Pixel ** extract_image(FILE * image) {
+    /*
+    .ppm file structure:
+    P6
+    <width> <height>
+    <max color value>
+    <binary RGB data>
+    */
+    //Skip the beginning of the ppm file which holds the tag P6
+    fseek(image, 3, SEEK_SET);
+    unsigned long width = read_num(image);
+    unsigned long height = read_num(image);
+    unsigned long maxval = read_num(image);
+
+    Pixel ** pixels = (Pixel**)malloc(sizeof(Pixel*) * height);
+    if(pixels == NULL) {
+        printf("\n\nError allocating memory.");
+        exit(1);
+    }
+    
+    for(int i = 0; i < height; i++) {
+        pixels[i] = (Pixel*)malloc(sizeof(Pixel) * width);
+        if(pixels[i] == NULL) {
+            printf("\n\nError allocating memory.");
+            exit(1);
+        }
+
+        for(int j = 0; j < width; j++) {
+            pixels[i][j].R = fgetc(image);
+            pixels[i][j].G = fgetc(image);
+            pixels[i][j].B = fgetc(image);
+        }
+    }
+    return pixels;
+}
