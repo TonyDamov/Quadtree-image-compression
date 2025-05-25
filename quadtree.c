23140f404ed510a404353fd43c91594cca6e4782
TonyDamov <antoan.damov@gmail.com>
2025-05-26 01:22:52 +0300

    Added image_detail() and patched bugs
    Added image_detail() to check whether the tree should split again or not. Patched for loop in init_node() to include the last row/column.

--- quadtree.c
+++ quadtree.c
@@ -4,6 +4,8 @@
 #include "quadtree.h"
 #include "readimage.c"
 
+//Exagarated for the purpose of testing
+#define COLOR_DIFF_TRESHOLD 384
 
 //Counting the pixels of the image from (left;top) = (0;0) to (right; bottom) = (width; height)
 
@@ -23,8 +25,8 @@ QuadtreeNode * init_node(Pixel ** pixels, long long nx, long long px, long long 
 
     //find mean color
     unsigned long long pixelsumR = 0, pixelsumG = 0, pixelsumB = 0;
-    for(int i = newnode->py; i < newnode->ny; i++) {
-        for(int j = newnode->nx; j < newnode->px; j++) {
+    for(int i = newnode->py; i <= newnode->ny; i++) {
+        for(int j = newnode->nx; j <= newnode->px; j++) {
             pixelsumR += pixels[i][j].R;
             pixelsumG += pixels[i][j].G;
             pixelsumB += pixels[i][j].B;
@@ -77,3 +79,35 @@ void split_tree(Pixel ** pixels , QuadtreeNode ** quadtree) {
     (*quadtree)->data.child[2] = newnode3;
     (*quadtree)->data.child[3] = newnode4;
 }
+
+int is_similar_color(Pixel ** pixels, Pixel meancolor, QuadtreeNode quadtree) {
+    int is_similar_color = 1;
+    for(int i = quadtree.py; i <= quadtree.ny && is_similar_color; i++) {
+        for(int j = quadtree.nx; j <= quadtree.px && is_similar_color; j++) {
+            int difference = abs(meancolor.R - pixels[i][j].R) + abs(meancolor.G - pixels[i][j].G) + abs(meancolor.B - pixels[i][j].B);
+            if(difference > COLOR_DIFF_TRESHOLD) {
+                is_similar_color = 0;
+            }
+        }
+    }
+    return is_similar_color;
+}
+
+void image_detail(Pixel ** pixels, QuadtreeNode ** quadtree) {
+    if(!is_similar_color(pixels, (*quadtree)->data.pixel, **quadtree)) {
+        split_tree(pixels, quadtree);
+        image_detail(pixels, &(*quadtree)->data.child[0]);
+        image_detail(pixels, &(*quadtree)->data.child[1]);
+        image_detail(pixels, &(*quadtree)->data.child[2]);
+        image_detail(pixels, &(*quadtree)->data.child[3]);
+    }
+}
+
+
+void main() {
+    FILE * image = fopen("paint.ppm", "r");
+    Pixel ** pixels = extract_image(image);
+    QuadtreeNode * quadtree = init_node(pixels, 0, image_width(image) - 1, image_height(image) - 1, 0);
+    fclose(image);
+    image_detail(pixels, &quadtree);
+} 
